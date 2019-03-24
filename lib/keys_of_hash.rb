eee3b439af0dafce9870f1fb5e2e48ed0814b4af
Jacob  Lam <jacob.lam.work@gmail.com>
2019-03-24 21:11:30 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -4,11 +4,10 @@ class Hash
   def keys_of(*arguments)
   output = []
   arguments.each do |args|
+  binding.pry
   self.each do | animal , location |
     if location == args
-      
       output  << animal
-      
     end 
   end
   output
